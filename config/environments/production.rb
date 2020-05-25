0fad737415b666578296ad0a61826e5f7520aaff
Maximilian Zweyer <maximilian.zweyer@studbocconi.it>
2020-05-25 18:47:02 +0200

    setup done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-
+  config.action_mailer.default_url_options = { host: 'https://airbnb-maxiking2r.herokuapp.com/'}
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
