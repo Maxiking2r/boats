45c0d611e7e5d713d2288ebad62913f5c413fb12
Frederike Soyka <f.m.soyka@googlemail.com>
2020-05-26 12:44:31 +0200

    Boats Controller Index Show

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,11 @@
 class BoatsController < ApplicationController
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+    @boat = Boat.find(params[:id])
+  end
 end
