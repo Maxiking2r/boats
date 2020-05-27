c60774010aa2f9d10874cc9daf1f6bdd78e252d7
Peter Hempel <hempel.pe@gmail.com>
2020-05-27 08:14:12 +0200

    BookingsController index/show defined and nested resources added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :boats, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :index, :show]
   end
 end
