0cab4c0e3e332d329f9c0d1dd462f5bcd3afda84
Maximilian Zweyer <maximilian.zweyer@studbocconi.it>
2020-05-27 15:59:23 +0200

    connected everything

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :boats, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:create]
+    resources :bookings, only: [:create, :new]
   end
   resources :users, only: [:show]
   resources :bookings, only: [:index, :show]
