074850a9bba67cee643b55eee0194ea8bd9c436e
Peter Hempel <hempel.pe@gmail.com>
2020-05-28 18:20:19 +0200

    boat name and renter/owner status of booking

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
     resources :bookings, only: [:index, :show, :edit, :update, :destroy]
     resources :boats, only: [:new, :create]
   end
-  
-  resources :bookings, only: [:index, :show]
+
+  resources :bookings, only: [:index, :show] do
+    member do
+      patch :accepted, :declined
+    end
+  end
 end
